231a5a535b05df00b62a74b32dfbe8031015eb91
David Bonnet <david@bonnet.cc>
2017-08-06 17:22:49 +0200

    Add tests for `defaultTraveler`

--- test/index.js
+++ test/index.js
@@ -1,7 +1,52 @@
+const fs = require('fs')
+const path = require('path')
+const normalizeNewline = require('normalize-newline')
 const { test } = require('tap')
+const { parse } = require('acorn')
 
-const { makeTraveler } = require('../dist/astravel')
+const { defaultTraveler, Found } = require('../dist/astravel')
 
-test('Initial', assert => {
+const DIRNAME = path.join(__dirname, 'samples')
+
+function getAst(filename, ecmaVersion, sourceType = 'module') {
+  const options = {
+    ecmaVersion,
+    sourceType,
+  }
+  const code = normalizeNewline(
+    fs.readFileSync(path.join(DIRNAME, filename), 'utf8')
+  )
+  const ast = parse(code, options)
+  return ast
+}
+
+test('Default traveler', assert => {
+  const ast = getAst('everything.js', 8)
+  defaultTraveler.go(ast)
+
+  assert.test('Deprecated', assert => {
+    const ast = getAst('deprecated.js', 5, 'script')
+    defaultTraveler.go(ast)
+    assert.end()
+  })
+
+  assert.test('Find node', assert => {
+    const returnNodeFinder = defaultTraveler.makeChild({
+      ReturnStatement(node, state) {
+        throw new Found(node, state)
+      },
+    })
+    const result = returnNodeFinder.find(ast)
+    assert.ok(result)
+    assert.ok(result.node)
+    assert.equal(result.node.type, 'ReturnStatement')
+    assert.end()
+  })
+
+  assert.test('Find node throws error', assert => {
+    assert.throws(() => defaultTraveler.find(), TypeError)
+    assert.end()
+  })
+
   assert.end()
 })
---
+++ test/samples/everything.js
@@ -0,0 +1,141 @@
+import a from 'a'
+import b, { c } from 'b'
+import { d as e } from 'c'
+import * as f from 'd'
+
+export { g } from 'd'
+export const h = a
+export * from 'e'
+
+export default class Class {
+  constructor() {
+    this.bound = () => {}
+    this.otherBound = (a) => ({})
+  }
+  static s() {}
+  m() {
+    new.target
+    this.isEverything = 42
+  }
+}
+
+const ClassExpression = class extends Class {}
+
+function callable(a, b, c = 42) {
+  let d = a
+  var e = b
+  const f = c
+  const {g, h, i} = e
+  const [j, k, l] = array
+  return c === 42 ? a : b
+}
+
+function *generator() {
+  yield
+  yield 42
+}
+
+async function asyncFunction() {
+  return await answer()
+}
+
+function branches() {
+  if (a > 0) {
+    callable(...a)
+  } else if (a < 0) {
+    callable(1)
+  } else {
+    callable(2)
+  }
+  if (a == 0) {}
+  label:
+  switch (a) {
+    case 1:
+      break label
+    default:
+      break
+  }
+  return
+}
+
+function loops() {
+  otherLabel:
+  for (let i = 0; i < a.length; i++) {
+    if (i < 10) {
+      continue otherLabel
+    } else {
+      continue
+    }
+  }
+  for ( ; ; ) {}
+  for (let name in object) {}
+  for (let name of array) {}
+  while(i < 0) {}
+  do {} while (i < 0)
+}
+
+function exceptions() {
+  try {
+    throw new Error()
+  } catch (error) {
+    callable(error)
+  } finally {
+    callable(42)
+  }
+  try {} catch (error) {}
+}
+
+const instance = new Class()
+
+const data = {
+  a,
+  b,
+  m() {},
+  instance,
+}
+
+const object = {
+  a: 42,
+  b: '42',
+  c: 1 + 1,
+  d: 2 * 2,
+  f: 2 / 2,
+  g: 3 - 2,
+  h: 2 % 2,
+  i: 1.5 | 0,
+  j: 1 ^ 2,
+  k: 1 & 2,
+  l: 'test' in data,
+  m: 1 == 2,
+  n: 1 != 2,
+  o: 1 === 2,
+  p: 1 !== 2,
+  q: 1 instanceof Number,
+  r: 1 || 2,
+  s: 1 && 2,
+  t: (1, 2),
+  u: 1 > 2,
+  v: 1 < 2,
+  w: 1 >= 2,
+  x: 1 <= 2,
+  y: 42 ** 2,
+}
+
+object.a = 42
+object.a += 42
+object.a -= 42
+object.a *= 42
+object.a /= 42
+object.a %= 1
+object.a++
+++object.a
+object.a--
+--object.a
+object.a = -object.a
+object.a **= 2
+
+const array = [1, 2, 3, , 4]
+
+const template = `Answer is ${42}`
+
+const formattedTemplate = format`Answer is ${a}`
